f0e80b33e2b488132e8f9a076208a8f7f1e0f1a0
delmond147 <delmondbongha147@gmail.com>
2022-08-05 05:17:39 +0100

    Udated Commits

---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,47 @@
+import { useReducer, useEffect } from 'react'
+import { useParams } from "react-router-dom"
+import axios from 'axios'
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'FETCH_REQUEST':
+            return { ...state, loading: true }
+        case 'FETCH_SUCCESS':
+            return { ...state, products: action.payload, loading: false }
+        case 'FETCH_FAIL':
+            return { ...state, loading: false, error: action.payload }
+        default:
+            return state
+    }
+}
+function ProductScreen() {
+    const params = useParams()
+    const { slug } = params
+
+    const [{ loading, error, product }, dispatch] = useReducer(reducer, {
+        products: [],
+        loading: true,
+        error: '',
+    })
+    // const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            dispatch({ type: 'FETCH_REQUEST' })
+            try {
+                const result = await axios.get(`/api/products/slug/${slug}`)
+                dispatch({ type: 'FETCH_SUCCESS', payload: result.data })
+            } catch (err) {
+                dispatch({ type: 'FETCH_FAIL', payload: err.message })
+            }
+            // setProducts(result.data)
+        }
+        fetchData()
+    }, [])
+    return (
+        <div>
+            <h1>{slug}</h1>
+        </div>
+    )
+}
+
+export default ProductScreen
