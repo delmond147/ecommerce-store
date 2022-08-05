fc8493dc14bc47cbeb3b63e10e5b5a9413be8404
delmond147 <delmondbongha147@gmail.com>
2022-08-05 05:18:01 +0100

    Update commit

--- README.md
+++ README.md
@@ -2,6 +2,48 @@
 
 # LESSONS
 
+### Create fronend
+
 1. Introduction
 2. install tools
 3. create react app
+
+### Create backend server
+
+1. run npm init in the root folder
+2. update package.json set type: module
+3. Add .js to imports
+4. npm install express
+5. create server.js
+6. add start command as node backend/server.js
+7. require express
+8. create route for /return backend is ready
+9. move products.js from frontend to backend
+10. create route for /api/products in server.js
+11. return products
+12. run npm start
+
+### Fetch Products from backend
+
+1. set proxy in package.json
+2. npm install axios
+3. use state hook
+4. use Effect hook
+5. use reducer hook
+
+### Manage State using reducers
+
+1. define reducer
+2. update fetch fetch
+3. get state from useReducer
+
+### Add bootstrap UI Framework
+
+1. npm install reac-bootstrap
+2. update App.js
+
+### Create Product and Rating Component
+
+1. Create Rating Component
+2. Create Product Component
+3. Use Rating Component in Product Component
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,35 @@
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from './screens/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+import { LinkContainer } from 'react-router-bootstrap'
 function App() {
   return (
-    <div>
-      <header>
-        <a href="">amazona</a>
-      </header>
-      <main>
-        list products
-      </main>
-    </div>
+    <BrowserRouter>
+      <div className="d-flex flex-column site-container">
+        <header>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>amazona</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
+        </header>
+        <main>
+          <Container>
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Container>
+        </main>
+        <footer className="text-center">
+          &copy;Copyright 2022 All rights Reserved || Dgitech
+        </footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
